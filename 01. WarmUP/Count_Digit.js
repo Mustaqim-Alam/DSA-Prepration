92c02ef94f9d0978743d9e94fd46be8df471d2a6
Md Mustaqim Alam <mustaqimalam@gmail.com>
2026-01-03 21:23:37 +0530

    Count digit problem solved

--- 01. WarmUP/Count_Digit.js
+++ 01. WarmUP/Count_Digit.js
@@ -1,12 +1,16 @@
 function countDigit(n) {
   let count = 0;
+
+  if (n == 0) return 1;
+
+  n = Math.abs(n);
   while (n > 0) {
     n = Math.floor(n / 10);
     count++;
   }
   return count;
 }
-let num = 465454645;
+let num = 45865;
 let result = countDigit(num);
 
 console.log(result);
